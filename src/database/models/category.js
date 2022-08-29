c6e06b2bb99cd3d1fa7bd0ecb00e3f4dcbf5557b
murilo-scapim <mhscapim@gmail.com>
2022-08-29 14:30:30 -0300

    Req 7 - created model category

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,23 +1,15 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Category.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Category',
-  });
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: DataTypes.STRING,
+  }, { timestamps: false, tableName: 'Categories'});
+
   return Category;
 };
+
+module.exports = Category;
